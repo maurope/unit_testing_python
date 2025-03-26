8790ebc19221b515582f8734f3d2e78fab8d1788
maurope <mauricio.penuela@hotmail.com>
2025-03-26 08:57:31 +0100

    test.py updated

--- test.py
+++ test.py
@@ -1,13 +1,17 @@
+# This is the main code
+
 def calculate_total(products):
     total = 0
     for product in products:
-        total *= product ['price']
+        total += product ['price']
     return total
+
+# These are the testing functions
 
 def test_calculate_total_with_empty_list():
     print('prueba')
 
-    assert calculate_total([]) == 0
+    assert calculate_total([]) == 0 # This line is the expected results
 
 
 def test_calculate_total_with_single_product():
@@ -16,9 +20,25 @@ def test_calculate_total_with_single_product():
             'name': 'Notebook', 'price':5
         }
     ]
-    assert calculate_total(products) == 10
+    assert calculate_total(products) == 5 # This line is the expected results
 
+def test_calculate_total_with_multiple_products():
+    products = [
+        {
+            'name': 'Notebook', 'price':5
+        },
+        {
+            'name': 'Book', 'price':5
+        },
+        {
+            'name': 'Pen', 'price':2
+        }
+    ]
+    assert calculate_total(products) == 12 # This line is the expected results
+
+# This is how we call the script to run in the terminal
 
 if __name__ == "__main__":
     test_calculate_total_with_empty_list()
     test_calculate_total_with_single_product()
+    test_calculate_total_with_multiple_products()
