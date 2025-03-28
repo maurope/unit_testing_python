f001471b16ef57bd8eece00b147b92225136d840
maurope <mauricio.penuela@hotmail.com>
2025-03-28 10:34:35 +0100

    Update bank account

--- test/test_bank_account.py
+++ test/test_bank_account.py
@@ -1,11 +1,20 @@
-import unittest
+
+import unittest, os
 from src.bank_account import BankAccount
 
 class BankAccountTest(unittest.TestCase):
 
-    def setUp(self) -> None:                        #The method setUp empty and is always called so we can use it
-        self.account = BankAccount(balance = 1000) 
+    def setUp(self) -> None:  #The method setUp empty and is always called so we can use it
+        self.account = BankAccount(balance = 1000, log_file = 'transaction_log.txt') 
 
+    def tearDown(self) -> None:
+        if os.path.exists(self.account.log_file):
+            os.remove(self.account.log_file)
+
+    def _count_lines(self, filename):
+        with open(filename, 'r') as f:
+            return len(f.readlines())
+        
     def test_deposit(self):
         new_balance = self.account.deposit(500)
         assert new_balance == 1500
@@ -16,3 +25,15 @@ class BankAccountTest(unittest.TestCase):
 
     def test_get_balance(self):
         assert self.account.get_balance() == 1000
+
+    def test_transaction_log(self):
+        self.account.deposit(500)
+        assert os.path.exists('transaction_log.txt')
+
+    def test_count_transaction(self):
+        assert self._count_lines(self.account.log_file) == 1
+        self.account.deposit(500)
+        assert self._count_lines(self.account.log_file) == 2
+
+        
+        
