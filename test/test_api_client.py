2aa7093eea1ee15c4845ad044b5ae7b03548422c
maurope <mauricio.penuela@hotmail.com>
2025-04-07 08:55:05 +0200

    Code update

--- test/test_api_client.py
+++ test/test_api_client.py
@@ -1,47 +1,44 @@
 import unittest, requests
+import unittest.mock
 from src.api_client import get_location
 from unittest.mock import patch
 
 
-class apiClientTests(unittest.TestCase):
+class ApiClientTests(unittest.TestCase):
 
-    @patch('src.api_client.requests.get')
+    @patch("src.api_client.requests.get")
     def test_get_location_returns_expected_data(self, mock_get):
         mock_get.return_value.status_code = 200
         mock_get.return_value.json.return_value = {
-            'countryName': 'USA',
-            'regionName': 'FLORIDA', 
-            'cityName': 'MIAMI'
+            "countryName": "USA",
+            "regionName": "FLORIDA",
+            "cityName": "MIAMI",
         }
-        result = get_location('8.8.8.8')
-        self.assertEqual(result.get('country'), 'USA')
-        self.assertEqual(result.get('region'), 'FLORIDA')
-        self.assertEqual(result.get('city'), 'MIAMI')
+        result = get_location("8.8.8.8")
+        self.assertEqual(result.get("country"), "USA")
+        self.assertEqual(result.get("region"), "FLORIDA")
+        self.assertEqual(result.get("city"), "MIAMI")
 
-        mock_get.assert_called_once_with('https://freeipapi.com/api/json/8.8.8.8')
+        mock_get.assert_called_once_with("https://freeipapi.com/api/json/8.8.8.8")
 
-
-    @patch('src.api_client.requests.get')
+    @patch("src.api_client.requests.get")
     def test_get_location_returns_side_effect(self, mock_get):
         mock_get.side_effect = [
-            requests.excepetions.RequestException('Service Unavailable'))
-
+            requests.exceptions.RequestException("Service Unavailable"),
+            unittest.mock.Mock(
+                status_code=200,
+                json=lambda: {
+                    "countryName": "USA",
+                    "regionName": "FLORIDA",
+                    "cityName": "MIAMI",
+                },
+            ),
         ]
-        mock_get.return_value.status_code = 200
-        mock_get.return_value.json.return_value = {
-            'countryName': 'USA',
-            'regionName': 'FLORIDA', 
-            'cityName': 'MIAMI'
-        }
 
-        with self.assertRaises(
-            requests.excepetions.RequestException('Service Unavailable')):
-            get_location('8.8.8.8')
+        with self.assertRaises(requests.exceptions.RequestException):
+            get_location("8.8.8.8")
 
-        result = get_location('8.8.8.8')
-
-    #    self.assertEqual(result.get('country'), 'USA')
-    #    self.assertEqual(result.get('region'), 'FLORIDA')
-    #    self.assertEqual(result.get('city'), 'MIAMI')
-
-        result = get_location('8.8.8.8')
+        result = get_location("8.8.8.8")
+        self.assertEqual(result.get("country"), "USA")
+        self.assertEqual(result.get("region"), "FLORIDA")
+        self.assertEqual(result.get("city"), "MIAMI")
