b3b77c5638b210c68f21784a28457e97098a9949
maurope <mauricio.penuela@hotmail.com>
2025-04-07 08:41:47 +0200

    Format code

--- test/test_api_client.py
+++ test/test_api_client.py
@@ -1,4 +1,4 @@
-import unittest
+import unittest, requests
 from src.api_client import get_location
 from unittest.mock import patch
 
@@ -19,3 +19,29 @@ class apiClientTests(unittest.TestCase):
         self.assertEqual(result.get('city'), 'MIAMI')
 
         mock_get.assert_called_once_with('https://freeipapi.com/api/json/8.8.8.8')
+
+
+    @patch('src.api_client.requests.get')
+    def test_get_location_returns_side_effect(self, mock_get):
+        mock_get.side_effect = [
+            requests.excepetions.RequestException('Service Unavailable'))
+
+        ]
+        mock_get.return_value.status_code = 200
+        mock_get.return_value.json.return_value = {
+            'countryName': 'USA',
+            'regionName': 'FLORIDA', 
+            'cityName': 'MIAMI'
+        }
+
+        with self.assertRaises(
+            requests.excepetions.RequestException('Service Unavailable')):
+            get_location('8.8.8.8')
+
+        result = get_location('8.8.8.8')
+
+    #    self.assertEqual(result.get('country'), 'USA')
+    #    self.assertEqual(result.get('region'), 'FLORIDA')
+    #    self.assertEqual(result.get('city'), 'MIAMI')
+
+        result = get_location('8.8.8.8')
