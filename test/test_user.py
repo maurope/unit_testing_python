70306c46f0435a3dc415c6cb1bfa678067fe68b7
maurope <mauricio.penuela@hotmail.com>
2025-04-10 08:52:22 +0200

    Update test_user

--- test/test_user.py
+++ test/test_user.py
@@ -1,6 +1,7 @@
 import unittest
 from faker import Faker
 from src.user import User
+from src.bank_account import BankAccount
 
 class UserTest(unittest.TestCase):
 
@@ -12,4 +13,19 @@ class UserTest(unittest.TestCase):
         name_generated = self.faker.name()
         email_generated = self.faker.email()
         user = User(name=name_generated, email=email_generated)
-        print(user.name, user.email)
+        self.assertEqual(user.name, name_generated)
+        self.assertEqual(user.email, email_generated)
+
+    def test_user_with_multiple_accounts(self):
+        user = User(name=self.faker.name(), email=self.faker.email())
+
+        for _ in range(3):
+            bank_account = BankAccount(
+                self.faker.random_int(min=100, max=2000, step=50),
+                log_file = self.faker.file_name(extension = '.txt')
+            )
+            user.add_account(account=bank_account)
+
+        expected_value = user.get_total_balance()
+        value = sum(account.get_balance() for account in user.accounts)
+        self.assertEqual(value, expected_value)
