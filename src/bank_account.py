283d76acf9406318b835e8c5fd01a9285394f9e3
maurope <mauricio.penuela@hotmail.com>
2025-04-22 09:51:24 +0200

    code update

--- src/bank_account.py
+++ src/bank_account.py
@@ -1,12 +1,12 @@
-from datetime import date
+from datetime import datetime
 from src.exceptions import InsufficientFundsError, WithdrawalTimeRestrictionError
 
+
 class BankAccount:
-    def __init__(self, balance=0, log_file =  None):
+    def __init__(self, balance=0, log_file=None):
         self.balance = balance
         self.log_file = log_file
-        self._log_transaction('Cuenta creada')
-
+        self._log_transaction("Cuenta creada")
 
     def _log_transaction(self, message):
         if self.log_file:
@@ -16,19 +16,23 @@ class BankAccount:
     def deposit(self, amount):
         if amount > 0:
             self.balance += amount
-            self._log_transaction(f'Deposited {amount}, New balance: {self.balance}')
+            self._log_transaction(f"Deposited {amount}. New balance: {self.balance}")
         return self.balance
 
     def withdraw(self, amount):
         now = datetime.now()
-        if now.hour < 8 and now.hour < 17:
-            raise WithdrawalTimeRestrictionError('Withdrawals are only allowed form 8am to 5pm')
+        if now.hour < 8 or now.hour > 17:
+            raise WithdrawalTimeRestrictionError("Withdrawals are only allowed from 8am to 5pm")
+
+        if amount > self.balance:
+            raise InsufficientFundsError(
+                f"Withdrawal of {amount} exceeds balance {self.balance}"
+            )
         if amount > 0:
             self.balance -= amount
-            self._log_transaction(f'Withdraw {amount}. New balance: {self.balance}')
+            self._log_transaction(f"Withdrew {amount}. New balance: {self.balance}")
         return self.balance
 
     def get_balance(self):
-        self._log_transaction(f'Checked balance.  New balance: {self.balance}')
+        self._log_transaction(f"Checked balance. Current balance: {self.balance}")
         return self.balance
-    
---
+++ test/test_pytest.py
@@ -0,0 +1,8 @@
+import pytest
+
+def test_sum():
+    a = 4
+    b = 8
+    assert a + b == 8
+
+    
